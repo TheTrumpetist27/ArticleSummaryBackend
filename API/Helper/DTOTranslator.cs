bec643fcb53ceea5cf8c9011b45c0d868edee49b
Kian Bruyenberg <kbruyenberg@gmail.com>
2025-06-16 13:49:02 +0200

    Update DTOTranslator.cs

--- API/Helper/DTOTranslator.cs
+++ API/Helper/DTOTranslator.cs
@@ -66,7 +66,8 @@ namespace API.Helper
             return new Comment
             {
                 Content = createCommentDTO.Text,
-                ArticleId = createCommentDTO.ArticleId
+                ArticleId = createCommentDTO.ArticleId,
+                CreatedAt = DateTime.UtcNow
             };
         }
     }
